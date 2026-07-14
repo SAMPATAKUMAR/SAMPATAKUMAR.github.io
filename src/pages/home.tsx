1738e775d0129d9f5629b1372997f3ebda926032
sampatakumar <148532254+sampatakumar@users.noreply.github.com>
2026-07-14 16:57:12 +0530

    LINKEDIN CARD

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -597,6 +597,9 @@ export default function Home() {
           </form>
 
         </div>
+        <script src="https://platform.linkedin.com/badges/js/profile.js" async defer type="text/javascript"></script>
+        <div class="badge-base LI-profile-badge" data-locale="en_US" data-size="medium" data-theme="dark" data-type="VERTICAL" data-vanity="sampatakumar-sv" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://in.linkedin.com/in/sampatakumar-sv?trk=profile-badge">Sampatakumar S V</a></div>
+              
 
       </section>
 
